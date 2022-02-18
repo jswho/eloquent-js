1d24b51fa1e040cdda348a1707a2d19878b9aea5
Oliver Arroyo <js_who@yahoo.com>
2022-02-18 20:56:35 +0000

    Function loopTriangle now takes a loops <Int> parameter

--- chapter-2/loop-triangle.js
+++ chapter-2/loop-triangle.js
@@ -1,15 +1,16 @@
-const loopTriangle = () => {
+const loopTriangle = (loops) => {
   let triangle = '';
   let counter = 1;
 
-  for (let i = 0; i < 7; i++) {
+  for (let i = 0; i < loops; i++) {
     for (let j = 0; j < counter; j++) {
       triangle += '#';
     }
+
     counter++;
     triangle += '\n';
   }
   return triangle;
 };
 
-console.log(loopTriangle());
+console.log(loopTriangle(3));
