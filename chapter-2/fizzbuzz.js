e0d1bf97317fa5c54e1fd0f317f6639c2a15b5ea
Oliver Arroyo <js_who@yahoo.com>
2022-02-19 11:33:50 +0000

    Numbers divisible by 5 prints 'Buzz'. Some errors, number 15

--- chapter-2/fizzbuzz.js
+++ chapter-2/fizzbuzz.js
@@ -9,6 +9,8 @@ const fizzbuzz = () => {
   for (let number = 1; number <= 100; number++) {
     if (number % 3 === 0) {
       console.log('Fizz');
+    } else if (number % 5 === 0) {
+      console.log('Buzz');
     } else {
       console.log(number);
     }
