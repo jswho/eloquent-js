4098491136e807bae77647f910cf524a2810ef20
Oliver Arroyo <js_who@yahoo.com>
2022-02-19 12:43:19 +0000

    A single line of symbols print.

--- chapter-2/chessboard.js
+++ chapter-2/chessboard.js
@@ -1,12 +1,20 @@
 /*
 - Print a grid that is 8 x 8
-  --> each line will alternate with " " and "#"
+  --> each line will alternate with " " and "#" and use the new line character
 
 - Refactor Ideas
   * change grid size
   * change alternating symbols
 */
 
-const chessboard = () => {};
+const chessboard = () => {
+  let grid = '';
+  let size = 8;
+
+  while (grid.length < size) {
+    grid += '#';
+  }
+  return grid;
+};
 
 console.log(chessboard());
