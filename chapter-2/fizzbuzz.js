d668f0856bdfc44952f6a7de3516d3664739ab70
Oliver Arroyo <js_who@yahoo.com>
2022-02-19 11:14:00 +0000

    Instructions added

--- chapter-2/fizzbuzz.js
+++ chapter-2/fizzbuzz.js
@@ -1,1 +1,10 @@
+/*
+- Print all numbers 1 - 100
+- Numbers divisible by 3, print "Fizz"
+- Numbers divisible by 5, print "Buzz"
+- Numbers divisbible by 3 and 5, print "FizzBuzz"
+*/
+
 const fizzbuzz = () => {};
+
+console.log(fizzbuzz());
