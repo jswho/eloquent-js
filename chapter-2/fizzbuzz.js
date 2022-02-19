15e568ec1a956f9dbd6633ae929c0b87a0da6377
Oliver Arroyo <js_who@yahoo.com>
2022-02-19 11:28:34 +0000

    Numbers divisible by 3, prints 'Fizz'

--- chapter-2/fizzbuzz.js
+++ chapter-2/fizzbuzz.js
@@ -2,12 +2,16 @@
 - Print all numbers 1 - 100
 - Numbers divisible by 3, print "Fizz"
 - Numbers divisible by 5, print "Buzz"
-- Numbers divisbible by 3 and 5, print "FizzBuzz"
+- Numbers divisible by 3 and 5, print "FizzBuzz"
 */
 
 const fizzbuzz = () => {
   for (let number = 1; number <= 100; number++) {
-    console.log(number);
+    if (number % 3 === 0) {
+      console.log('Fizz');
+    } else {
+      console.log(number);
+    }
   }
 };
 
