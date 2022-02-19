5560b801aea240c245abf5a7a389bdb67c94e3a3
Oliver Arroyo <js_who@yahoo.com>
2022-02-19 11:37:17 +0000

    Numbers divisible by 3 and 5, prints 'FizzBuzz'

--- chapter-2/fizzbuzz.js
+++ chapter-2/fizzbuzz.js
@@ -7,7 +7,9 @@
 
 const fizzbuzz = () => {
   for (let number = 1; number <= 100; number++) {
-    if (number % 3 === 0) {
+    if (number % 3 === 0 && number % 5 === 0) {
+      console.log('FizzBuzz');
+    } else if (number % 3 === 0) {
       console.log('Fizz');
     } else if (number % 5 === 0) {
       console.log('Buzz');
