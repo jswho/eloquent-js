46271c7bff0f34e74aa1b501b1900b2a87b8d85f
Oliver Arroyo <js_who@yahoo.com>
2022-02-19 12:29:10 +0000

    Instructions added

--- chapter-2/chessboard.js
+++ chapter-2/chessboard.js
@@ -1,3 +1,12 @@
+/*
+- Print a grid that is 8 x 8
+  --> each line will alternate with " " and "#"
+
+- Refactor Ideas
+  * change grid size
+  * change alternating symbols
+*/
+
 const chessboard = () => {};
 
 console.log(chessboard());
