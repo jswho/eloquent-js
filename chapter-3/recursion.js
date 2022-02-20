3976940caa9448487374628a63f78503c3367d6e
Oliver Arroyo <js_who@yahoo.com>
2022-02-20 18:09:50 +0000

    Empty function with instructions.

---
+++ chapter-3/recursion.js
@@ -0,0 +1,11 @@
+/*
+Define a function that takes a number, recursively check if even and return a boolean.
+
+- 0 is even
+- 1 is odd
+- For any other number, use number - 2
+*/
+
+const isEven = (number) => {};
+
+console.log(isEven());
