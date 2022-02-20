e408aa77a5a0188114cdba79910c025746d064e9
Oliver Arroyo <js_who@yahoo.com>
2022-02-20 23:13:42 +0000

    Recursive attempt does not work. Incorrect counter number is returned due to resetting the variable when recursing.

--- chapter-3/bean-counting.js
+++ chapter-3/bean-counting.js
@@ -2,6 +2,31 @@
 - Return a number, that has counted the number of uppercase "B"'s in the string
 */
 
-const countBs = (string) => {};
+const countBs = (str) => {
+  const checkB = 'B';
 
-console.log(countBs('BBC'));
+  if (str[str.length - 1] === checkB) {
+    counter();
+  }
+
+  if (str.length === 0) {
+    return counter(true);
+  } else {
+    countBs(str.substring(0, str.length - 1));
+  }
+};
+
+const count = () => {
+  let memory = 0;
+
+  return (getCount = false) => {
+    if (getCount) {
+      return memory;
+    } else {
+      return memory++;
+    }
+  };
+};
+const counter = count();
+
+console.log(countBs('ABC'));
