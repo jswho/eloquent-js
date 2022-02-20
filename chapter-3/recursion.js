53acc288a638dece034f3b15fd710d0eae963093
Oliver Arroyo <js_who@yahoo.com>
2022-02-20 18:13:15 +0000

    Recursive function solution to check if a number is even.

--- chapter-3/recursion.js
+++ chapter-3/recursion.js
@@ -6,6 +6,15 @@ Define a function that takes a number, recursively check if even and return a bo
 - For any other number, use number - 2
 */
 
-const isEven = (number) => {};
+const isEven = (number) => {
+  if (number === 0) {
+    return true;
+  } else if (number === 1) {
+    return false;
+  } else {
+    return isEven(number - 2);
+  }
+};
 
-console.log(isEven());
+console.log(isEven(50)); // true
+console.log(isEven(75)); // false
