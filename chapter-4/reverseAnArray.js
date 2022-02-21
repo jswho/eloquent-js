5713c826f70cb029bd028be6cc87be7eebb0b9aa
Oliver Arroyo <js_who@yahoo.com>
2022-02-21 17:06:31 +0000

    revereArray returns new reversed array. Includes a reference check between old and new array.

--- chapter-4/reverseAnArray.js
+++ chapter-4/reverseAnArray.js
@@ -8,9 +8,17 @@ Mutable
 Reverse an array in place and return the reversed array
 */
 
-const reverseArray = (array) => {};
+const reverseArray = (array) => {
+  const newArray = [];
+
+  for (let i = array.length - 1; i >= 0; i--) {
+    newArray.push(array[i]);
+  }
+  console.log('Check: ' + (array === newArray));
+  return newArray;
+};
 
 const reverseArrayInPlace = (array) => {};
 
 console.log(reverseArray(['A', 'B', 'C'])); // ["C", "B", "A"]
-console.log(reverseArrayInPlace([1, 2, 3, 4, 5])); // [5, 4, 3, 2, 1]
+// console.log(reverseArrayInPlace([1, 2, 3, 4, 5])); // [5, 4, 3, 2, 1]
