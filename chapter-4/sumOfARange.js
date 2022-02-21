34b7b8cfb7b85405da6fb26acfaf7879e66457e1
Oliver Arroyo <js_who@yahoo.com>
2022-02-21 13:16:52 +0000

    Empty range function added with instructions for exercise.

---
+++ chapter-4/sumOfARange.js
@@ -0,0 +1,12 @@
+/*
+Range
+- Define a function with 2 arguments, start and end.
+- Return an array of all numbers from start to end.
+
+Sum
+- Define a function that sums the entire array of numbers from function range.
+*/
+
+const range = (start, end) => {};
+
+console.log(range(1, 10));
