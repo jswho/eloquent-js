9f1950fbe9f54e1d65a7cfb53196dd47266aa835
Oliver Arroyo <js_who@yahoo.com>
2022-02-21 15:41:54 +0000

    Refactor range function to include a step argument. Additional function included, for for-loop termination.

--- chapter-4/sumOfARange.js
+++ chapter-4/sumOfARange.js
@@ -5,12 +5,16 @@ Range
 
 Sum
 - Define a function that sums the entire array of numbers from function range.
+
+Extra
+- Provide a third argument to range function, that gives the number to count by.
 */
 
-const range = (start, end) => {
+const range = (start, end, step) => {
   const numbers = [];
+  const terminator = (i) => (step > 0 ? i <= end : i >= end);
 
-  for (let i = start; i <= end; i++) {
+  for (let i = start; terminator(i); i += step) {
     numbers.push(i);
   }
   return numbers;
@@ -25,5 +29,7 @@ const sum = (numbers) => {
   return value;
 };
 
-// console.log(range(1, 10));
-console.log(sum(range(1, 10)));
+// console.log(range(1, 10, 2));
+console.log(range(5, 2, -1));
+// console.log(sum(range(1, 10)));
+console.log(sum(range(5, 2, -1)));
