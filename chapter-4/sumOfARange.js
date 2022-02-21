30ce9d0a37edf1e6d4433da1395aa194a906ed81
Oliver Arroyo <js_who@yahoo.com>
2022-02-21 14:43:34 +0000

    Range function uses for loop

--- chapter-4/sumOfARange.js
+++ chapter-4/sumOfARange.js
@@ -7,6 +7,13 @@ Sum
 - Define a function that sums the entire array of numbers from function range.
 */
 
-const range = (start, end) => {};
+const range = (start, end) => {
+  const numbers = [];
+
+  for (let i = start; i <= end; i++) {
+    numbers.push(i);
+  }
+  return numbers;
+};
 
 console.log(range(1, 10));
