fbb7e96ca4b98e4c030090b0a05f4369a0408d03
Oliver Arroyo <js_who@yahoo.com>
2022-02-21 16:41:01 +0000

    Immutable and mutable empty reverse functions setup, including instructions.

---
+++ chapter-4/reverseAnArray.js
@@ -0,0 +1,16 @@
+/*
+Do not use JS reverse().
+
+Immutable
+Reverse an array and return a new reversed array
+
+Mutable
+Reverse an array in place and return the reversed array
+*/
+
+const reverseArray = (array) => {};
+
+const reverseArrayInPlace = (array) => {};
+
+console.log(reverseArray(['A', 'B', 'C'])); // ["C", "B", "A"]
+console.log(reverseArrayInPlace([1, 2, 3, 4, 5])); // [5, 4, 3, 2, 1]
