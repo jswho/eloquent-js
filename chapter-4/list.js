b12a3bc4b035f3552b79f03b55499cc96dd45a3f
Oliver Arroyo <js_who@yahoo.com>
2022-02-21 23:23:47 +0000

    I struggled here. I resorted to looking at the solution.

--- chapter-4/list.js
+++ chapter-4/list.js
@@ -1,3 +1,13 @@
-const arrayToList = () => {};
+const arrayToList = (array) => {
+  let list = null;
+
+  for (let i = array.length - 1; i >= 0; i--) {
+    list = {
+      value: array[i],
+      rest: list,
+    };
+  }
+  return list;
+};
 
 console.log(arrayToList([10, 20])); // {value: 10, rest: {value: 20, rest: null}}
