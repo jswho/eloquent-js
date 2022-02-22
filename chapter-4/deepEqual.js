08172efef0107de2df340baf83f02407e2b8d06b
Oliver Arroyo <js_who@yahoo.com>
2022-02-22 01:01:34 +0000

    Recursive function for checking type 'object' in properties

--- chapter-4/deepEqual.js
+++ chapter-4/deepEqual.js
@@ -2,9 +2,17 @@
 - Should return a boolean if both objects have the same properties
 */
 
-const deepEqual = () => {};
+const deepEqual = (obj1, obj2) => {
+  for (key in obj1) {
+    console.log(obj1);
+    if (typeof obj1[key] === 'object') {
+      return deepEqual(obj1[key], obj2);
+    }
+  }
+};
 
 let obj = { here: { is: 'an' }, object: 2 };
-console.log(deepEqual(obj, obj)); // true
-console.log(deepEqual(obj, { here: 1, object: 2 })); // false
-console.log(deepEqual(obj, { here: { is: 'an' }, object: 2 })); // true
+// console.log(deepEqual(obj, obj)); // true
+// console.log(deepEqual(obj, { here: 1, object: 2 })); // false
+// console.log(deepEqual(obj, { here: { is: 'an' }, object: 2 })); // true
+console.log(deepEqual({ a: { b: { c: { here: 22 } } } }, obj));
