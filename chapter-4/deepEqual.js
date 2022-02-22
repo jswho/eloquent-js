9ff18d9d927536b91c9ed0d0b2ea36c2fdeab86f
Oliver Arroyo <js_who@yahoo.com>
2022-02-22 13:46:05 +0000

    Returns correct booleans for example tests, but breaks for one of mine.

--- chapter-4/deepEqual.js
+++ chapter-4/deepEqual.js
@@ -4,15 +4,20 @@
 
 const deepEqual = (obj1, obj2) => {
   for (key in obj1) {
-    console.log(obj1);
+    // check if property is of type 'object', if so call function recursively
     if (typeof obj1[key] === 'object') {
-      return deepEqual(obj1[key], obj2);
+      return deepEqual(obj1[key], obj2[key]);
+
+      // check if current values inside both objects match
+    } else if (obj1[key] !== obj2[key]) {
+      return false;
     }
   }
+  return true;
 };
 
 let obj = { here: { is: 'an' }, object: 2 };
-// console.log(deepEqual(obj, obj)); // true
-// console.log(deepEqual(obj, { here: 1, object: 2 })); // false
-// console.log(deepEqual(obj, { here: { is: 'an' }, object: 2 })); // true
-console.log(deepEqual({ a: { b: { c: { here: 22 } } } }, obj));
+console.log(deepEqual(obj, obj)); // true
+console.log(deepEqual(obj, { here: 1, object: 2 })); // false
+console.log(deepEqual(obj, { here: { is: 'an' }, object: 2 })); // true
+// console.log(deepEqual({ a: { b: { c: { here: 22 } } } }, obj));
