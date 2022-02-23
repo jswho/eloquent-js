df6c9c1b898121b13c873ea2d68b39a91762c775
Oliver Arroyo <js_who@yahoo.com>
2022-02-23 20:51:39 +0000

    Test case missing an enclosing [].

--- chapter-5/flatten.js
+++ chapter-5/flatten.js
@@ -5,4 +5,9 @@ Using the concat method, flatten a multi array in to a single array.
 const flatten = (multiArray) => {};
 
 console.log(flatten([[1, 2, 3], [4, 5], [6]])); // [1,2,3,4,5,6]
-console.log(flatten([11, 12], [13, 14, [15, 16, 17, 18, [19, 20]]])); // [11,12,13,14,15,16,17,18,19,20]
+console.log(
+  flatten([
+    [11, 12],
+    [13, 14, [15, 16, 17, 18, [19, 20]]],
+  ])
+); // [11,12,13,14,15,16,17,18,19,20]
