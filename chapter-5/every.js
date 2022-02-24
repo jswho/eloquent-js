96ba37053b6d8fe91df00b9cc49389c366fa30e2
Oliver Arroyo <js_who@yahoo.com>
2022-02-24 15:59:30 +0000

    Used the hint/solution in the end!

--- chapter-5/every.js
+++ chapter-5/every.js
@@ -3,17 +3,27 @@
 - Use loop and then some method
 */
 
-const every = (array, test) => {
-  let result = true;
+// Loop
+// const every = (array, test) => {
+//   let result = true;
 
-  if (array.length) {
-    for (element of array) {
-      result = test(element);
-    }
-  }
-  return result;
+//   if (array.length) {
+//     for (element of array) {
+//       result = test(element);
+//     }
+//   }
+//   return result;
+// };
+
+// Some
+const everySome = (array, test) => {
+  return !array.some((element) => !test(element));
 };
 
-console.log(every([1, 3, 5], (n) => n < 10)); // true
-console.log(every([2, 4, 16], (n) => n < 10)); // false
-console.log(every([], (n) => n < 10)); // true
+// console.log(every([1, 3, 5], (n) => n < 10)); // true
+// console.log(every([2, 4, 16], (n) => n < 10)); // false
+// console.log(every([], (n) => n < 10)); // true
+
+console.log(everySome([1, 3, 5], (n) => n < 10)); // true
+console.log(everySome([2, 4, 16], (n) => n < 10)); // false
+console.log(everySome([], (n) => n < 10)); // true
