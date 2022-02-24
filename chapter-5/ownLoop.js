58868c82e43b65e419e874ce17d91b2bea580d36
Oliver Arroyo <js_who@yahoo.com>
2022-02-24 10:50:15 +0000

    Empty function setup.

---
+++ chapter-5/ownLoop.js
@@ -0,0 +1,19 @@
+/*
+Develop own loop function
+*/
+
+const loop = (value, test, update, body) => {};
+
+console.log(
+  loop(
+    3,
+    (n) => n > 0,
+    (n) => n - 1,
+    console.log
+  )
+);
+/*
+3
+2
+1
+*/
